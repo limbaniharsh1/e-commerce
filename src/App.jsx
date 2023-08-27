15c7580fafabd3a5f08493efe8b802d2dacd5893
harsh limbani <limbaniharsh1@gmail.com>
2023-08-27 20:06:22 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <div>
         {/* <HomePage/> */}
         {/* <Product/> */}
-        {/* <ProductDetails/> */}
-        <Cart/>
+        <ProductDetails/>
+        {/* <Cart/> */}
       </div>
       <Footer/>
     </div>
